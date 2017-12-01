d58f64b8959b8e0e2d25df0f12b60815088a95ba
Dante Budin <dante.budin@immalle.eu>
2017-12-01 11:05:49 +0100

    leeftijd DUMS

--- bereken hoe oud je bent DUMS/Program.cs
+++ bereken hoe oud je bent DUMS/Program.cs
@@ -9,6 +9,11 @@ namespace bereken_hoe_oud_je_bent_DUMS
     {
         static void Main(string[] args)
         {
+            DateTime jeuj = new DateTime(2000, 01, 17, 04, 0, 0);
+            TimeSpan oud = DateTime.Now - jeuj;
+
+            Console.WriteLine("{0} dagen, {1} uur, {2} minuten en {3} seconden om precies te zijn...", oud.Days, oud.Hours, oud.Minutes, oud.Seconds);
+            Console.WriteLine("Oftewel {0} seconden...", oud.TotalSeconds);
         }
     }
 }
